4727c9871e934724d27ab8ff417b54cccbf32ef1
PHJoon <phj9503@gmail.com>
2024-04-19 00:12:19 +0900

    [Feat] index.html, index.js 라우팅 로직 추가

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import Login from './pages/Login.js';
+import Main from './pages/Main.js';
+import Profile from './pages/Profile.js';
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+// 동적라우팅 추가 필요
+const router = async () => {
+  const routes = {
+    '/': Login,
+    '/main': Main,
+    '/profile': Profile,
+  };
+
+  const page = new routes[location.pathname]();
+
+  document.querySelector('#app').innerHTML = await page.render();
+};
+
+window.addEventListener('popstate', router);
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('[data-link]')) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+  router();
+});
