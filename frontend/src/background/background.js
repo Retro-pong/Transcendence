24d252faf6c876528c7eb76a10031db078f59b57
PHJoon <phj9503@gmail.com>
2024-04-23 01:23:01 +0900

    [Design] background v2 추가

--- frontend/src/background/background.js
+++ frontend/src/background/background.js
@@ -5,10 +5,40 @@
 const drawBackground = () => {
   const background = document.getElementById('background');
 
-  const top = document.createElement('div');
-  top.className = 'top';
-  background.appendChild(top);
+  // v1
+  // const top = document.createElement('div');
+  // top.className = 'top';
+  // background.appendChild(top);
 
+  // const left = document.createElement('div');
+  // left.className = 'left';
+  // background.appendChild(left);
+
+  // const right = document.createElement('div');
+  // right.className = 'right';
+  // background.appendChild(right);
+
+  // const bottom = document.createElement('div');
+  // bottom.className = 'bottom';
+  // background.appendChild(bottom);
+
+  // for (let i = 1; i < 6; i += 1) {
+  //   const topDiv = document.createElement('div');
+  //   const leftDiv = document.createElement('div');
+  //   const rightDiv = document.createElement('div');
+  //   const bottomDiv = document.createElement('div');
+
+  //   topDiv.className = `top-element-${i}`;
+  //   leftDiv.className = `left-element-${i}`;
+  //   rightDiv.className = `right-element-${i}`;
+  //   bottomDiv.className = `bottom-element-${i}`;
+
+  //   top.appendChild(topDiv);
+  //   left.appendChild(leftDiv);
+  //   right.appendChild(rightDiv);
+  //   bottom.appendChild(bottomDiv);
+
+  // v2
   const left = document.createElement('div');
   left.className = 'left';
   background.appendChild(left);
@@ -17,25 +47,15 @@ const drawBackground = () => {
   right.className = 'right';
   background.appendChild(right);
 
-  const bottom = document.createElement('div');
-  bottom.className = 'bottom';
-  background.appendChild(bottom);
-
-  for (let i = 1; i < 6; i += 1) {
-    const topDiv = document.createElement('div');
+  for (let i = 1; i < 10; i += 1) {
     const leftDiv = document.createElement('div');
     const rightDiv = document.createElement('div');
-    const bottomDiv = document.createElement('div');
 
-    topDiv.className = `top-element-${i}`;
     leftDiv.className = `left-element-${i}`;
     rightDiv.className = `right-element-${i}`;
-    bottomDiv.className = `bottom-element-${i}`;
 
-    top.appendChild(topDiv);
     left.appendChild(leftDiv);
     right.appendChild(rightDiv);
-    bottom.appendChild(bottomDiv);
   }
 };
 
