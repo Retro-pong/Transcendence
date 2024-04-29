9b26887859be765983f51f3df659249a5a2b59e0
hyobb109 <hyobb109@gmail.com>
2024-04-30 00:03:39 +0900

    [FE] refactor: 페이지 경로 수정

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return `<nav class="nav">
-            <a href="/" class="nav__link" data-link>Login</a>
-            <a href="/main" class="nav__link" data-link>Main</a>
+            <a href="/login" class="nav__link" data-link>Login</a>
+            <a href="/" class="nav__link" data-link>Main</a>
             <a href="/profile" class="nav__link" data-link>Profile</a>
           </nav>`;
 };
