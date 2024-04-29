96e28e9d80bbd66d30052114a42e1047c411b823
이원진 <wonjilee@student.42seoul.kr>
2024-04-29 17:32:20 +0900

    [BE] chore: dockerfile, cmd.sh 추가

--- backend/init.sh
+++ backend/init.sh
@@ -1,10 +1,10 @@
 #!/bin/source
 
 # 가상 환경 생성
-python3 -m venv myvenv
+python3 -m venv venv
 
 # 가상 환경 활성화
-source myvenv/bin/activate
+source venv/bin/activate
 
 # requirements.txt에 나열된 패키지 설치
 pip install -r requirements.txt
