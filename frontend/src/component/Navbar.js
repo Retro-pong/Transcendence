0a7cbd8ac28a4ce1da1a07b945a45cd6e26bcd38
hyobb109 <hyobb109@gmail.com>
2024-04-30 02:20:17 +0900

    [FE] feat: NavLink 컴포넌트 구현

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,8 +1,25 @@
+import NavLink from './NavLink.js';
+
 const NavBar = () => {
-  return `<nav class="nav">
-            <a href="/login" class="nav__link" data-link>Login</a>
-            <a href="/" class="nav__link" data-link>Main</a>
-            <a href="/profile" class="nav__link" data-link>Profile</a>
+  const links = [
+    {
+      text: 'Login',
+      path: '/login',
+    },
+    {
+      text: 'Main',
+      path: '/',
+    },
+    {
+      text: 'Profile',
+      path: '/profile',
+    },
+  ];
+
+  const NavLinks = links.map((link) => NavLink(link).outerHTML).join('');
+
+  return `<nav class="nav flex-column">
+            ${NavLinks}
           </nav>`;
 };
 
