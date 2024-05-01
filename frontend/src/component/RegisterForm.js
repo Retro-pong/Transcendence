ae57938e4ad378dcc242c0bf11c4d351e85d11b2
PHJoon <phj9503@gmail.com>
2024-05-01 17:36:40 +0900

    [FE] Design: 회원가입 모달 스타일 수정

--- frontend/src/component/RegisterForm.js
+++ frontend/src/component/RegisterForm.js
@@ -2,42 +2,42 @@ const RegisterForm = () => {
   return `
     <form id="registerForm" class="form">
       <div class="container">
-        <div class="row my-4 mx-4">
+        <div class="row my-5 mx-4">
           <div class="col-5">
               <label for="email" class="form-label">EMAIL</label>
           </div>
-          <div class="col-2">:</div>
-          <div class="col-5">
-            <input type="text" id="eamil" class="form-control" placeholder="name@mail.com"></input>
+          <div class="col-1 d-flex align-items-center">:</div>
+          <div class="col-6">
+            <input type="text" id="eamil" class="form-control register-input" placeholder="name@mail.com"></input>
           </div>
         </div>
-        <div class="row mb-4 mx-4">
+        <div class="row mb-5 mx-4">
           <div class="col-5">
           <label for="nick" class="form-label">NICKNAME</label>
           </div>
-          <div class="col-2">:</div>
-          <div class="col-5">
-            <input type="text" id="nick" class="form-control" placeholder="nickname"></input>
+          <div class="col-1 d-flex align-items-center">:</div>
+          <div class="col-6">
+            <input type="text" id="nick" class="form-control register-input" placeholder="nickname"></input>
           </div>
         </div>
 
-        <div class="row mb-4 mx-4">
+        <div class="row mb-5 mx-4">
           <div class="col-5">
             <label for="password" class="form-label">PASSWORD</label>
           </div>
-          <div class="col-2">:</div>
-          <div class="col-5">
-            <input type="password" id="password" class="form-control"></input>
+          <div class="col-1 d-flex align-items-center">:</div>
+          <div class="col-6">
+            <input type="password" id="password" class="form-control register-input"></input>
           </div>
         </div>
 
-        <div class="row mb-4 mx-4">
+        <div class="row mb-5 mx-4">
           <div class="col-5">
-            <label for="password-re" class="form-label">PASSWORD(RE)</label>
+            <label for="password-re" class="form-label">PASSWORD</label>
           </div>
-          <div class="col-2">:</div>
-          <div class="col-5">
-            <input type="password" id="password-re" class="form-control"></input>
+          <div class="col-1 d-flex align-items-center">:</div>
+          <div class="col-6">
+            <input type="password" id="password-re" class="form-control register-input"></input>
           </div>
         </div>
       </div>
--- frontend/src/component/modal/ModalComponent.js
+++ frontend/src/component/modal/ModalComponent.js
@@ -4,15 +4,15 @@ const ModalComponent = ({ title, modalId, content, buttons }) => {
   return `
     <div class="modal fade modal-lg" id="${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog rounded">
-        <div class="modal-content bg-black">
-          <div class="modal-header border-0 d-flex flex-column bg-dark">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            <h1 class="modal-title" id="exampleModalLabel">${title}</h1>
+        <div class="modal-content vh-75 bg-black">
+          <div class="modal-header border-0 d-flex flex-column bg-black">
+          <button type="button" class="btn-close d-flex align-items-center justify-content-center fs-16" data-bs-dismiss="modal" aria-label="Close">X</button>
+            <h1 class="modal-title fs-15" id="exampleModalLabel">${title}</h1>
           </div>
           <div class="modal-body">
             ${content}
           </div>
-          <div class="modal-footer border-0">
+          <div class="modal-footer bg-black h-25 border-0 d-flex justify-content-center align-items-center">
             ${buttons.map((button) => button).join('')}
           </div>
         </div>
