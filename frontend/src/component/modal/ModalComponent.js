1503f119dac35006372c8f18381cca4232d6db15
PHJoon <phj9503@gmail.com>
2024-05-01 16:07:58 +0900

    [FE] feat: 모달 컴포넌트 추가

---
+++ frontend/src/component/modal/ModalComponent.js
@@ -0,0 +1,23 @@
+import { Modal } from 'bootstrap';
+
+const ModalComponent = ({ title, modalId, content, buttons }) => {
+  return `
+    <div class="modal fade modal-lg" id="${modalId}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog rounded">
+        <div class="modal-content bg-black">
+          <div class="modal-header border-0">
+            <h1 class="modal-title" id="exampleModalLabel">${title}</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ${content}
+          </div>
+          <div class="modal-footer border-0">
+            ${buttons.map((button) => button).join('')}
+          </div>
+        </div>
+      </div>
+    </div>`;
+};
+
+export default ModalComponent;
