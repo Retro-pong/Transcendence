5ab31375eedc2067b6ec8bce41a09db15d80157a
hyobb109 <hyobb109@gmail.com>
2024-05-03 01:44:15 +0900

    [FE] design: input border style 수정

--- frontend/src/component/form/RegisterFormItem.js
+++ frontend/src/component/form/RegisterFormItem.js
@@ -6,7 +6,7 @@ const RegisterFormItem = (classList, labelId, content, type, placeholder) => {
       </div>
       <div class="col-1 d-flex align-items-center fs-8">:</div>
       <div class="col-6 d-flex align-items-center justify-content-center fs-8">
-        <input type="${type}" id="${labelId}" class="form-control border-2 border-top-0 border-start-0 border-end-0 h-100 fs-8 bg-transparent" placeholder="${placeholder}"></input>
+        <input type="${type}" id="${labelId}" class="form-control h-100 fs-8 bg-transparent" placeholder="${placeholder}" />
       </div>
     </div>
   `;
