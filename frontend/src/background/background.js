55ddd2c4a53ce4b6371c105118ad6d84eb9936a9
PHJoon <phj9503@gmail.com>
2024-05-02 14:53:24 +0900

    [FE] design, fix: 배경 css 값 수정

--- frontend/src/background/background.js
+++ frontend/src/background/background.js
@@ -3,8 +3,21 @@ const drawBackground = () => {
 
   const left = ['1', '6', '6-5', '12', '3', '9', '10', '1', '8', '11'];
   const right = ['98', '93', '93-5', '87', '96', '90', '89', '98', '91', '88'];
-  const top = ['10', '5', '65', '76', '35', '60', '1', '75', '20', '50'];
-  const top2 = ['30', '25', '85', '76', '35', '60', '20', '75', '40', '50'];
+  const leftTop = ['10', '5', '65', '76', '35', '60', '1', '75', '20', '50'];
+  const leftTop2 = ['30', '25', '85', '76', '35', '60', '20', '75', '40', '50'];
+  const rightTop = ['50', '20', '75', '1', '60', '35', '76', '65', '5', '10'];
+  const rightTop2 = [
+    '50',
+    '40',
+    '75',
+    '20',
+    '60',
+    '35',
+    '76',
+    '85',
+    '25',
+    '30',
+  ];
   const size = [
     'w h-xxxs',
     'w h-xs',
@@ -39,9 +52,9 @@ const drawBackground = () => {
     const lSecondChild = document.createElement('div');
     const lThirdChild = document.createElement('div');
 
-    lFirst.className = `ele l${left[i]} t${top[i]} ${size[i]} ${color[i]}`;
-    lSecond.className = `ele l${left[i]} t${top2[i]} ${size[i]} ${color[i]} d1`;
-    lThird.className = `ele l${left[i]} t${top2[i]} ${size[i]} ${color[i]} d2`;
+    lFirst.className = `ele l${left[i]} t${leftTop[i]} ${size[i]} ${color[i]}`;
+    lSecond.className = `ele l${left[i]} t${leftTop2[i]} ${size[i]} ${color[i]} d1`;
+    lThird.className = `ele l${left[i]} t${leftTop2[i]} ${size[i]} ${color[i]} d2`;
 
     lFirst.appendChild(lFirstChild);
     lSecond.appendChild(lSecondChild);
@@ -58,9 +71,9 @@ const drawBackground = () => {
     const rSecondChild = document.createElement('div');
     const rThirdChild = document.createElement('div');
 
-    rFirst.className = `ele l${right[i]} t${top[i]} ${size[i]} ${color[i]}`;
-    rSecond.className = `ele l${right[i]} t${top2[i]} ${size[i]} ${color[i]} d1`;
-    rThird.className = `ele l${right[i]} t${top2[i]} ${size[i]} ${color[i]} d2`;
+    rFirst.className = `ele l${right[i]} t${rightTop[i]} ${size[i]} ${color[i]}`;
+    rSecond.className = `ele l${right[i]} t${rightTop2[i]} ${size[i]} ${color[i]} d1`;
+    rThird.className = `ele l${right[i]} t${rightTop2[i]} ${size[i]} ${color[i]} d2`;
 
     rFirst.appendChild(rFirstChild);
     rSecond.appendChild(rSecondChild);
