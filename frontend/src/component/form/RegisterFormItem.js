5fcc5ec97bd492e9b01acbbe4c822ac652d84d52
hyobb109 <hyobb109@gmail.com>
2024-05-03 01:05:44 +0900

    [FE] design: input 스타일 수정

--- frontend/src/component/form/RegisterFormItem.js
+++ frontend/src/component/form/RegisterFormItem.js
@@ -4,9 +4,9 @@ const RegisterFormItem = (classList, labelId, content, type, placeholder) => {
       <div class="col-5 d-flex align-items-center justify-content-start fs-8">
         <label for="${labelId}" class="form-label">${content}</label>
       </div>
-      <div class="col-1 d-flex align-items-center">:</div>
+      <div class="col-1 d-flex align-items-center fs-8">:</div>
       <div class="col-6 d-flex align-items-center justify-content-center fs-8">
-        <input type="${type}" id="${labelId}" class="form-control border-0 h-100 fs-8 bg-transparent" placeholder="${placeholder}"></input>
+        <input type="${type}" id="${labelId}" class="form-control border-2 border-top-0 border-start-0 border-end-0 h-100 fs-8 bg-transparent" placeholder="${placeholder}"></input>
       </div>
     </div>
   `;
