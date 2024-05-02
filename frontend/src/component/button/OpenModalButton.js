ccb61eeaa44423796612442f292a84cb82c1e23a
hyobb109 <hyobb109@gmail.com>
2024-05-03 03:22:18 +0900

    [FE] comment: 주석 삭제

--- frontend/src/component/button/OpenModalButton.js
+++ frontend/src/component/button/OpenModalButton.js
@@ -1,5 +1,4 @@
 const OpenModalButton = ({ text, classList, modalId }) => {
-  console.log(classList);
   return `
     <button type="button" class="${classList}" data-bs-toggle="modal" data-bs-target="${modalId}">
       ${text}
