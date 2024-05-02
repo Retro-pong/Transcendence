562d45e03ac0fcd51d92e7c9bc13901ff0f78405
PHJoon <phj9503@gmail.com>
2024-05-02 17:11:08 +0900

    [FE] feat: 수락, 취소 버튼 컴포넌트 추가

---
+++ frontend/src/component/button/AcceptButton.js
@@ -0,0 +1,7 @@
+const AcceptButton = () => {
+  return `
+    <button class="btn btn-friend-accept rounded-5 fs-8">OK</button>
+  `;
+};
+
+export default AcceptButton;
---
+++ frontend/src/component/button/RejectButton.js
@@ -0,0 +1,7 @@
+const RejectButton = () => {
+  return `
+  <button class="btn btn-friend-reject rounded-5 fs-8">NO</button>
+  `;
+};
+
+export default RejectButton;
