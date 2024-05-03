9f3eb4b60f50a4307532445b8cbfab312a543a14
PHJoon <phj9503@gmail.com>
2024-05-03 20:06:14 +0900

    [FE] design: css border-width 변수 커스텀 추가

--- frontend/src/component/modal/ModalComponent.js
+++ frontend/src/component/modal/ModalComponent.js
@@ -15,7 +15,7 @@ const ModalComponent = ({
   return `
   <div class="modal fade modal-lg" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content bg-black modal-border-${borderColor}">
+      <div class="modal-content bg-black border-10 modal-border-${borderColor}">
         <div class="modal-header pt-2 m-0 d-flex flex-column rounded-top-5 border-0 bg-black">
           <button type="button" class="btn-close p-0 border-0 d-flex align-items-center justify-content-center fs-15" data-bs-dismiss="modal" aria-label="Close">X</button>
           <h1 class="modal-title fs-16" id="${modalId}Label">${title}</h1>
