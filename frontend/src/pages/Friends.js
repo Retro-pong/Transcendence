4854e40d9317062e027184a7372ad9ea1efb418c
PHJoon <phj9503@gmail.com>
2024-05-03 20:07:12 +0900

    [FE] feat: friends 페이지에 친구대기,추가 버튼 및 모달 추가

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -1,4 +1,8 @@
 import PageComponent from '@component/PageComponent.js';
+import OpenModalButton from '@component/button/OpenModalButton';
+import ModalComponent from '@component/modal/ModalComponent';
+import FriendWaitList from '@component/contents/FriendWaitList';
+import FriendSearch from '@component/contents/FriendSearch';
 
 class Friends extends PageComponent {
   constructor() {
@@ -7,11 +11,42 @@ class Friends extends PageComponent {
   }
 
   async render() {
+    const FriendWaitBtn = OpenModalButton({
+      text: '> WAITING',
+      classList: 'btn btn-no-outline-hover fs-8',
+      modalId: '#friendWaitModal',
+    });
+    const FriendAddBtn = OpenModalButton({
+      text: '> ADD',
+      classList: 'btn btn-no-outline-hover fs-8',
+      modalId: '#friendAddModal',
+    });
+    const FriendWaitModal = ModalComponent({
+      borderColor: 'mint',
+      title: 'WAITING',
+      modalId: 'friendWaitModal',
+      content: FriendWaitList(),
+      buttonList: [],
+    });
+    const FriendAddModal = ModalComponent({
+      borderColor: 'pink',
+      title: 'ADD FRIEND',
+      modalId: 'friendAddModal',
+      content: FriendSearch(),
+      buttonList: [],
+    });
+
+    const BtnList = [
+      `${FriendWaitBtn} ${FriendWaitModal}`,
+      `${FriendAddBtn} ${FriendAddModal}`,
+    ];
     return `
       <h1>Friends</h1>
       <p>
         This is Friends Page
       </p>
+      <div class="d-flex justify-content-between">
+        ${BtnList.join('')}
       `;
   }
 }
