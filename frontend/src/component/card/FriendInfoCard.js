88aecfb57ead6d3f71aaf11ded3c0e2fe4ac4c17
hyobb109 <hyobb109@gmail.com>
2024-05-06 02:20:20 +0900

    [FE] feat: 더미 생성 및 친구 카드 컴포넌트 구현

--- frontend/src/component/card/FriendInfoCard.js
+++ frontend/src/component/card/FriendInfoCard.js
@@ -1,25 +1,43 @@
-const FriendInfoCard = () => {
+import { Tooltip } from 'bootstrap'; // eslint-disable-line no-unused-vars
+
+const FriendInfoCard = ({
+  id,
+  name,
+  win,
+  lose,
+  comment,
+  status,
+  profileImg,
+}) => {
+  const statusColor = status === 'online' ? 'bg-success' : 'bg-danger';
+  const commentMessage = comment || '...';
+  // const Comment =
+  //   comment.length > 11
+  //     ? `<p class="card-text text-truncate" data-bs-toggle="tooltip" data-bs-custom-class="tooltip-comment" data-bs-title="${comment}">comment: ${comment}</p>`
+  //     : `<p class="card-text">comment: ${comment}</p>`;
   return `
-    <div class="card text-bg-dark border-5 border-light rounded-5 p-3" style="width: 30rem;">
+    <div id="${id}" class="card text-bg-dark border-5 border-light justify-content-center rounded-5 p-3 my-1" style="width: 33rem; min-width: 33rem;">
       <div class="row g-0">
-        <div class="col-md-8">
+        <div class="col-md-8 align-self-center">
           <div class="card-body fs-4 text-break">
-            <div class="row card-text">
-              <p class="card-text ">name: name</p>
+            <div class="row">
+              <p class="card-text ">name: ${name}</p>
             </div>
             <div class="row">
-              <p class="card-text ">win/lose: 1/99</p>
+              <p class="card-text ">win/lose: ${win}/${lose}</p>
             </div>
             <div class="row">
-              <p class="card-text ">comment: hello</p>
+              <p class="card-text text-truncate">comment: ${comment}</p>
             </div>
             <div class="row">
-              <p class="card-text position-relative">status: online <span class="position-absolute p-2 text-bg-secondary rounded-circle bg-success ms-1" style="top: 27%;" /></p>
+              <p class="card-text position-relative">status: ${status} <span class="position-absolute p-2 text-bg-secondary rounded-circle ${statusColor} ms-1" style="top: 27%;" /></p>
             </div>
           </div>
         </div>
         <div class="col-md-4 align-content-center">
-          <img src="https://www.blueconomy.co.kr/news/photo/202402/2399_3001_921.png" class="img-fluid" alt="profile" />
+          <div class="ratio ratio-1x1">
+            <img src=${profileImg} class="rounded-3" data-bs-toggle="tooltip" data-bs-custom-class="tooltip-comment" data-bs-title="${commentMessage}" alt="profile" style="object-fit: cover;" />
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -11,6 +11,72 @@ class Friends extends PageComponent {
     this.setTitle('Friends');
   }
 
+  // TODO: 친구 목록 api 요청
+  async getFriends() {
+    return [
+      {
+        id: 1,
+        name: 'hyobicho',
+        win: 99,
+        lose: 1,
+        comment: 'hello world',
+        status: 'online',
+        profileImg:
+          'https://www.blueconomy.co.kr/news/photo/202402/2399_3001_921.png',
+      },
+      {
+        id: 2,
+        name: 'hyungjpa',
+        win: 10,
+        lose: 1,
+        comment: '',
+        status: 'offline',
+        profileImg:
+          'https://www.blueconomy.co.kr/news/photo/202402/2399_3001_921.png',
+      },
+      {
+        id: 3,
+        name: 'wonjilee',
+        win: 1,
+        lose: 99,
+        comment: '동해물과 백두산이',
+        status: 'online',
+        profileImg:
+          'https://www.blueconomy.co.kr/news/photo/202402/2399_3001_921.png',
+      },
+      {
+        id: 4,
+        name: 'subinlee',
+        win: 5,
+        lose: 5,
+        comment: '🥳🥳🥳 this is a very long comment hahaha',
+        status: 'offline',
+        profileImg:
+          'https://www.blueconomy.co.kr/news/photo/202402/2399_3001_921.png',
+      },
+      {
+        id: 5,
+        name: 'kitty',
+        win: 1511,
+        lose: 95,
+        comment: 'meooooow',
+        status: 'online',
+        profileImg:
+          'https://i.pinimg.com/236x/91/be/98/91be9861cc5cef854ccc3f8ece507918.jpg',
+      },
+      {
+        id: 6,
+        name: 'puppy',
+        win: 124,
+        lose: 94,
+        comment: 'woff woff',
+        status: 'offline',
+        profileImg:
+          'https://img4.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202105/25/holapet/20210525081724428qquq.jpg',
+      },
+    ];
+  }
+
   async render() {
     const FriendWaitBtn = OpenModalButton({
       text: '> WAITING',
@@ -41,12 +107,15 @@ class Friends extends PageComponent {
       `${FriendWaitBtn} ${FriendWaitModal}`,
       `${FriendAddBtn} ${FriendAddModal}`,
     ];
+    const dummyFriends = await this.getFriends();
     return `
       <h1>Friends</h1>
       <div class="d-flex justify-content-between">
         ${BtnList.join('')}
       </div>
-      ${FriendInfoCard()}
+      <div class="d-flex flex-wrap justify-content-evenly h-75 overflow-auto">
+        ${dummyFriends.map((friend) => FriendInfoCard(friend)).join('')}
+      </div>
       `;
   }
 }
