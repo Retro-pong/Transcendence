c1c51f5ce353fbca0329fdc6b22dd07c61007444
PHJoon <phj9503@gmail.com>
2024-05-06 16:19:56 +0900

    [FE] design: UserProfileInfo 추가

---
+++ frontend/src/component/contents/UserProfileInfo.js
@@ -0,0 +1,26 @@
+const UserProfileInfo = ({ nickname, email, winLose, comment }) => {
+  return `
+	  <div class="row">
+	  	<div class="col-4">NICKNAME</div>
+	  	<div class="col-1">:</div>
+	  	<div class="col-7">${nickname}</div>
+	  </div>
+	  <div class="row">
+	  	<div class="col-4">EMAIL</div>
+	  	<div class="col-1">:</div>
+	  	<div class="col-7">${email}</div>
+	  </div>
+	  <div class="row">
+	  	<div class="col-4">WIN / LOSE</div>
+	  	<div class="col-1">:</div>
+	  	<div class="col-7">${winLose}</div>
+	  </div>
+	  <div class="row">
+	  	<div class="col-4">COMMENT</div>
+	  	<div class="col-1">:</div>
+	  	<div class="col-7">${comment}</div>
+	  </div>
+	`;
+};
+
+export default UserProfileInfo;
