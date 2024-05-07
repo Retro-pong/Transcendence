e6708c14ee142c5d272999008171e40f03f4076c
hyobb109 <hyobb109@gmail.com>
2024-05-07 21:23:31 +0900

    [FE] feat: 토스트 컴포넌트 구현

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,9 @@ import PageComponent from '@component/PageComponent.js';
 import RegisterFormItem from '@component/form/RegisterFormItem.js';
 import Header from '@component/text/Header';
 import LoginPageButtons from '@component/button/LoginPageButtons';
+import ToastComponent from '@component/toast/ToastComponent';
+import { Toast } from 'bootstrap';
+import Regex from '@/constants/Regex';
 
 class Login extends PageComponent {
   constructor() {
@@ -24,17 +27,36 @@ class Login extends PageComponent {
         <div class="p-5">
           ${LoginPageButtons()}
         </div>
+          ${ToastComponent({ id: 'invalidInputToast', message: 'please enter your email and password' })}
+          ${ToastComponent({ id: 'invalidEmailToast', message: 'Invalid Email address' })}
       </div>
       `;
   }
 
+  async getAccessToken({ email, password }) {
+    console.log(email, password);
+  }
+
   async afterRender() {
+    const invalidInputToast = Toast.getOrCreateInstance(
+      document.getElementById('invalidInputToast')
+    );
+    const invalidEmailToast = Toast.getOrCreateInstance(
+      document.getElementById('invalidEmailToast')
+    );
     // TODO: login api 요청 & 에러 처리
     document.getElementById('loginBtn').addEventListener('click', async () => {
       const email = document.getElementById('email-login').value;
       const password = document.getElementById('password-login').value;
-      if (!email || !password) return;
-      console.log(`email login`);
+      if (!email || !password) {
+        invalidInputToast.show();
+        return;
+      }
+      if (!email.match(Regex.email)) {
+        invalidEmailToast.show();
+        return;
+      }
+      await this.getAccessToken({ email, password });
     });
 
     // TODO: 42 login api 요청 & 에러 처리
