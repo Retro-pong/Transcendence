12488aae3b0aed41eb1903b66be75a221f92f4ee
hyobb109 <hyobb109@gmail.com>
2024-05-08 17:45:19 +0900

    [FE] fix: passcode 패턴 수정

--- frontend/src/component/form/LoginForm.js
+++ frontend/src/component/form/LoginForm.js
@@ -5,7 +5,7 @@ const LoginForm = () => {
       <form id="loginForm" class="form-floating p-4">
         <div class="row justify-content-center">
           <div class="col-6">
-            <input type="text" pattern="[0-9]+" minlength="6" maxlength="6" id="passcode" class="form-control text-center fs-8 bg-transparent" placeholder="passcode" />
+            <input type="text" minlength="6" maxlength="6" id="passcode" class="form-control text-center fs-8 bg-transparent" placeholder="passcode"/>
           </div>
           <div id="timer" class="col-2 align-self-center fs-7 text-bg-info rounded-2 bg-opacity-10">
             5:00
