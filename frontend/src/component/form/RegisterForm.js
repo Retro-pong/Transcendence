ab2bfe8e8d0df81cd83d28a742958403c4a41393
PHJoon <phj9503@gmail.com>
2024-05-09 17:28:02 +0900

    [FE] fix: registerFormItem id 명 수정

--- frontend/src/component/form/RegisterForm.js
+++ frontend/src/component/form/RegisterForm.js
@@ -7,7 +7,7 @@ const RegisterForm = () => {
         ${RegisterFormItem('row my-5 mx-3', 'email', 'EMAIL', 'text', 'email')}
         ${RegisterFormItem('row mb-5 mx-3', 'nick', 'NICK', 'text', 'nickname')}
         ${RegisterFormItem('row mb-5 mx-3', 'password', 'PASS', 'password', 'PASSWORD')}
-        ${RegisterFormItem('row mb-5 mx-3', 'password-re', 'PASS(re)', 'password', 'PASSWORD')}
+        ${RegisterFormItem('row mb-5 mx-3', 'passwordRe', 'PASS(re)', 'password', 'PASSWORD')}
       </div>
     </form>
   `;
