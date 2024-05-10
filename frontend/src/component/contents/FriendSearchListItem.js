20c828fdbb6cb6fa813bddc5d9ae40216cef6c31
PHJoon <phj9503@gmail.com>
2024-05-10 20:04:17 +0900

    [FE] refactor, feat: FriendSearchListItem 템플릿 리터럴 -> 돔 요소 직접 생성 방식으로 변경, 버튼 이벤트 추가

--- frontend/src/component/contents/FriendSearchListItem.js
+++ frontend/src/component/contents/FriendSearchListItem.js
@@ -1,15 +1,36 @@
-import BasicButton from '@component/button/BasicButton.js';
+import Fetch from '@/utils/Fetch';
 
-const FriendSearchListItem = ({ friend }) => {
-  return `
-    <div class="row my-2 px-5">
-      <div class="col-10 fs-10">${friend}</div>
-      ${BasicButton({
-        text: '+',
-        classList: 'col-2 fs-8',
-      })}
-    </div>
-    `;
+const FriendSearchListItem = ({ nick }) => {
+  const item = document.createElement('div');
+  const col = document.createElement('div');
+  const addFriendBtn = document.createElement('button');
+
+  item.classList.add('row', 'my-2', 'px-5');
+  col.classList.add('col-10', 'fs-10');
+  col.innerText = nick;
+  addFriendBtn.classList.add(
+    'btn',
+    'fs-8',
+    'col-2',
+    'btn-no-outline-hover',
+    'bg-transparent'
+  );
+  addFriendBtn.innerText = '+';
+
+  item.appendChild(col);
+  item.appendChild(addFriendBtn);
+
+  // TODO: api 연결 후 로직 추가
+  addFriendBtn.addEventListener('click', async () => {
+    try {
+      await Fetch.post('/friend-add-new', { nick });
+      console.log('친구 추가 성공');
+    } catch (error) {
+      console.error('친구 추가 실패');
+    }
+  });
+
+  return item;
 };
 
 export default FriendSearchListItem;
