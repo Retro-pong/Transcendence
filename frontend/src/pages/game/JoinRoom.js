d89dea93c373a7ca6916521e06a82eb44def30f7
hyobb109 <hyobb109@gmail.com>
2024-05-10 16:46:22 +0900

    [FE] feat: room list 요청

--- frontend/src/pages/game/JoinRoom.js
+++ frontend/src/pages/game/JoinRoom.js
@@ -1,4 +1,7 @@
 import PageComponent from '@component/PageComponent.js';
+import NavLink from '@component/navigation/NavLink';
+import Fetch from '@/utils/Fetch';
+// import BasicButton from '@component/button/BasicButton';
 
 class JoinRoom extends PageComponent {
   constructor() {
@@ -6,12 +9,34 @@ class JoinRoom extends PageComponent {
     this.setTitle('Join Room');
   }
 
+  async getRoomList() {
+    return Fetch.get('/roomList');
+  }
+
   async render() {
+    const roomList = await this.getRoomList();
+    const RoomLinks = roomList
+      .map((room) => {
+        return NavLink({
+          text: `[ ${room.title} ] (${room.currentParty}/${room.totalParty})`,
+          path: '/game', // path: `/game/room/${room.id}`,
+          classList: 'btn btn-no-outline-hover fs-11 btn-arrow',
+          disabled: false,
+        }).outerHTML;
+      })
+      .join('');
     return `
-      <h1 class="display-1 text-center">[ Room List ]</h1>
-      <p>
-        this is a join room page
-      </p>
+      <div class="container h-100 p-3 game-room-border">
+        <h1 class="display-1 text-center">[ Room List ]</h1>
+        <div class="d-flex justify-content-center h-75 overflow-auto">
+          <div class="d-flex flex-column">
+            ${RoomLinks}
+          </div>
+        </div>
+        <div class="d-flex justify-content-center">
+          pagination
+        </div>
+      </div>
       `;
   }
 }
