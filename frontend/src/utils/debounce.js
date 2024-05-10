7fa5319808aaf3b102aa22f6731b099614d98262
PHJoon <phj9503@gmail.com>
2024-05-10 19:10:19 +0900

    [FE] feat: debounce 추가

---
+++ frontend/src/utils/debounce.js
@@ -0,0 +1,11 @@
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+export default debounce;
