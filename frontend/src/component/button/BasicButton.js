828b7b1884e9d93df2985d22b435efd8bb18b001
PHJoon <phj9503@gmail.com>
2024-05-10 17:11:04 +0900

    [FE] feat: BasicButton data-set 정의하는 인자 추가

--- frontend/src/component/button/BasicButton.js
+++ frontend/src/component/button/BasicButton.js
@@ -1,10 +1,10 @@
-const BasicButton = ({ id, text, classList = '', disabled = false }) => {
+const BasicButton = ({ id, text, classList = '', disabled = false, data = '' }) => {
   const btnClass = `btn btn-no-outline-hover ${classList}`;
   return disabled
     ? `
-    <button type="button" id=${id} class="${btnClass}" disabled> ${text} </button>
+    <button type="button" id=${id} class="${btnClass}" disabled ${data}> ${text} </button>
   `
-    : `<button type="button" id=${id} class="${btnClass}"> ${text} </button>`;
+    : `<button type="button" id=${id} class="${btnClass} ${data}"> ${text} </button>`;
 };
 
 export default BasicButton;
