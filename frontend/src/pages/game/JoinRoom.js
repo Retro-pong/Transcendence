996bb8d2f2fd705bb4928f0b2b533f19bac57222
hyobb109 <hyobb109@gmail.com>
2024-05-14 21:14:32 +0900

    [FE] feat: room mode tab 추가

--- frontend/src/pages/game/JoinRoom.js
+++ frontend/src/pages/game/JoinRoom.js
@@ -24,7 +24,6 @@ class JoinRoom extends PageComponent {
           text: `[ ${room.title} ] (${room.currentParty}/${totalParty})`,
           path: '/game/waiting',
           classList: 'btn btn-no-outline-hover fs-11 btn-arrow',
-          disabled: false,
         }).outerHTML;
       })
       .join('');
@@ -38,20 +37,23 @@ class JoinRoom extends PageComponent {
       classList:
         'fs-2 position-absolute top-0 end-0 mt-2 me-2 btn-no-outline-hover',
     });
-
     return `
       <div class="container h-100 p-3 game-room-border">
         <div class="d-flex justify-content-center position-relative">
           <h1 class="display-1 text-center">[ Room List ]</h1>
           ${reloadRoomBtn}
         </div>
-        <div class="d-flex justify-content-center h-75 overflow-auto">
-          <div id="joinRoomBody" class="d-flex flex-column">
+        <nav class="nav nav-tabs">
+          <button class="btn btn-outline-light fs-1 active" id="rumble-tab" data-bs-toggle="tab" type="button">Rumble</button>
+          <button class="btn btn-outline-light fs-1" id="tournament-tab" data-bs-toggle="tab" type="button" >Tournament</button>
+        </nav>
+        <div class="d-flex justify-content-center overflow-auto" style="height: 70%;">
+          <div id="joinRoomBody" class="tab-pane active d-flex flex-column">
             ${RoomLinks}
           </div>
         </div>
         <div class="d-flex justify-content-center">
-          pagination
+          >> 1 / 3 <<
         </div>
       </div>
       `;
@@ -62,6 +64,19 @@ class JoinRoom extends PageComponent {
     const joinRoomBody = document.getElementById('joinRoomBody');
 
     reloadRoomBtn.addEventListener('click', async () => {
+      joinRoomBody.innerHTML = await this.getRoomList();
+    });
+
+    const rumbleTab = document.getElementById('rumble-tab');
+    const tournamentTab = document.getElementById('tournament-tab');
+
+    rumbleTab.addEventListener('click', async () => {
+      this.mode = 'rumble';
+      joinRoomBody.innerHTML = await this.getRoomList();
+    });
+
+    tournamentTab.addEventListener('click', async () => {
+      this.mode = 'tournament';
       joinRoomBody.innerHTML = await this.getRoomList();
     });
   }
