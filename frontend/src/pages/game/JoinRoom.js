a3e6aa7653ffcb4e21cb483def1c02cb6b654860
hyobb109 <hyobb109@gmail.com>
2024-05-14 19:25:21 +0900

    [FE] feat: room list 새로고침 기능 추가

--- frontend/src/pages/game/JoinRoom.js
+++ frontend/src/pages/game/JoinRoom.js
@@ -6,16 +6,22 @@ import Fetch from '@/utils/Fetch';
 class JoinRoom extends PageComponent {
   constructor() {
     super();
+    this.mode = 'rumble';
     this.setTitle('Join Room');
   }
 
-  async getRoomLinks() {
-    const roomList = await Fetch.get('/rumbleList');
+  async getRoomList() {
+    const roomList =
+      this.mode === 'rumble'
+        ? await Fetch.get('/rumbleList')
+        : await Fetch.get('/tournamentList');
+
+    const totalParty = this.mode === 'rumble' ? 2 : 4;
 
     return roomList
       .map((room) => {
         return NavLink({
-          text: `[ ${room.title} ] (${room.currentParty}/${room.totalParty})`,
+          text: `[ ${room.title} ] (${room.currentParty}/${totalParty})`,
           path: '/game/waiting',
           classList: 'btn btn-no-outline-hover fs-11 btn-arrow',
           disabled: false,
@@ -25,9 +31,9 @@ class JoinRoom extends PageComponent {
   }
 
   async render() {
-    const RoomLinks = await this.getRoomLinks();
-    const reloadBtn = BasicButton({
-      id: 'reloadBtn',
+    const RoomLinks = await this.getRoomList();
+    const reloadRoomBtn = BasicButton({
+      id: 'reloadRoomBtn',
       text: '> Reload',
       classList:
         'fs-2 position-absolute top-0 end-0 mt-2 me-2 btn-no-outline-hover',
@@ -37,10 +43,10 @@ class JoinRoom extends PageComponent {
       <div class="container h-100 p-3 game-room-border">
         <div class="d-flex justify-content-center position-relative">
           <h1 class="display-1 text-center">[ Room List ]</h1>
-          ${reloadBtn}
+          ${reloadRoomBtn}
         </div>
         <div class="d-flex justify-content-center h-75 overflow-auto">
-          <div class="d-flex flex-column">
+          <div id="joinRoomBody" class="d-flex flex-column">
             ${RoomLinks}
           </div>
         </div>
@@ -49,6 +55,15 @@ class JoinRoom extends PageComponent {
         </div>
       </div>
       `;
+  }
+
+  async afterRender() {
+    const reloadRoomBtn = document.getElementById('reloadRoomBtn');
+    const joinRoomBody = document.getElementById('joinRoomBody');
+
+    reloadRoomBtn.addEventListener('click', async () => {
+      joinRoomBody.innerHTML = await this.getRoomList();
+    });
   }
 }
 
