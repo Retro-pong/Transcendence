c18f0c50ac830c6d3d35c2ba2f9a3b058c8ba6c9
hyobb109 <hyobb109@gmail.com>
2024-05-14 18:49:30 +0900

    [FE] design: reload 버튼 추가

--- frontend/src/pages/game/JoinRoom.js
+++ frontend/src/pages/game/JoinRoom.js
@@ -1,5 +1,6 @@
 import PageComponent from '@component/PageComponent.js';
 import NavLink from '@component/navigation/NavLink';
+import BasicButton from '@component/button/BasicButton';
 import Fetch from '@/utils/Fetch';
 
 class JoinRoom extends PageComponent {
@@ -9,7 +10,7 @@ class JoinRoom extends PageComponent {
   }
 
   async getRoomLinks() {
-    const roomList = await Fetch.get('/roomList');
+    const roomList = await Fetch.get('/rumbleList');
 
     return roomList
       .map((room) => {
@@ -25,10 +26,19 @@ class JoinRoom extends PageComponent {
 
   async render() {
     const RoomLinks = await this.getRoomLinks();
+    const reloadBtn = BasicButton({
+      id: 'reloadBtn',
+      text: '> Reload',
+      classList:
+        'fs-2 position-absolute top-0 end-0 mt-2 me-2 btn-no-outline-hover',
+    });
 
     return `
       <div class="container h-100 p-3 game-room-border">
-        <h1 class="display-1 text-center">[ Room List ]</h1>
+        <div class="d-flex justify-content-center position-relative">
+          <h1 class="display-1 text-center">[ Room List ]</h1>
+          ${reloadBtn}
+        </div>
         <div class="d-flex justify-content-center h-75 overflow-auto">
           <div class="d-flex flex-column">
             ${RoomLinks}
