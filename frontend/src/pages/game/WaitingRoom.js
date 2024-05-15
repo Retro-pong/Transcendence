b0728a25db6f91219c7e3cdb60b820d6f3c6fe83
hyobb109 <hyobb109@gmail.com>
2024-05-16 00:07:40 +0900

    [FE] design: waiting room 레이아웃

--- frontend/src/pages/game/WaitingRoom.js
+++ frontend/src/pages/game/WaitingRoom.js
@@ -1,15 +1,18 @@
 import PageComponent from '@component/PageComponent.js';
 import PlayerCard from '@component/card/PlayerCard';
+import BasicButton from '@component/button/BasicButton';
 
 class WaitingRoom extends PageComponent {
   constructor() {
     super();
-    this.setTitle('Create Room');
+    this.setTitle('Waiting Room');
   }
 
   async render() {
     const params = new URLSearchParams(document.location.search);
     const TITLE = params.get('title');
+    // TODO: 소켓으로 게임 방 정보 받아오기, 참가자가 아니면 홈으로 리다이렉트
+    const ROLE = 'host';
     const dummyPlayers = [
       {
         id: 1,
@@ -45,15 +48,27 @@ class WaitingRoom extends PageComponent {
         `;
       })
       .join('');
+    const StartButton = BasicButton({
+      id: 'startBtn',
+      text: '<< Start Game! >>',
+      classList: 'btn btn-no-outline-hover fs-10',
+      disabled: ROLE !== 'host',
+    });
+
     return `
       <div class="container h-100 p-3 game-room-border">
-        <h1 class="display-1 text-center">Welcome to<br /> [ ${TITLE} ]</h1>
-        <div class="d-flex justify-content-center align-items-center h-75">
-          <div class="container text-center w-100">
-            <div class="row row-cols-1 row-cols-md-2 g-3">
-              ${PlayerCards}
+        <div class="d-flex flex-column h-100">
+          <h1 class="display-1 text-center">Welcome to<br /> [ ${TITLE} ]</h1>
+          <div class="d-flex justify-content-center align-items-center h-75">
+            <div class="container text-center w-100">
+              <div class="row row-cols-1 row-cols-md-2 g-3">
+                ${PlayerCards}
+              </div>
             </div>
           </div>
+          <div class="d-flex justify-content-center align-items-center h-25">
+            ${StartButton}
+          <div>
         </div>
       </div>
       `;
