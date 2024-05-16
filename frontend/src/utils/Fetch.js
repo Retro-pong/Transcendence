c7aa1e60b6ca92805638e1142ea015190c3f233f
hyobb109 <hyobb109@gmail.com>
2024-05-16 20:48:17 +0900

    [FE] fix: Fetch url 및 throw response 수정

--- frontend/src/utils/Fetch.js
+++ frontend/src/utils/Fetch.js
@@ -1,5 +1,5 @@
 class Fetch {
-  static #BASE_URL = 'http://localhost:8080';
+  static #BASE_URL = 'http://localhost:80';
 
   static #headers = {
     'Content-Type': 'application/json',
@@ -20,26 +20,26 @@ class Fetch {
   }
 
   static async get(url) {
-    const response = await fetch(`${this.#BASE_URL}${url}`, {
+    const response = await fetch(`${this.#BASE_URL}${url}/`, {
       method: 'GET',
       headers: this.#headers,
       credentials: this.#credentials,
     });
     if (!response.ok) {
-      throw response.status;
+      throw response;
     }
     return response.json();
   }
 
   static async post(url, body) {
-    const response = await fetch(`${this.#BASE_URL}${url}`, {
+    const response = await fetch(`${this.#BASE_URL}${url}/`, {
       method: 'POST',
       headers: this.#headers,
       credentials: this.#credentials,
       body: JSON.stringify(body),
     });
     if (!response.ok) {
-      throw response.status;
+      throw response;
     }
     return response.json();
   }
