b40b9da3c7922006f9e47ff0b1aab2ae31d15114
이원진 <wonjilee@student.42seoul.kr>
2024-05-17 15:51:46 +0900

    [BE] Feat: user profile api add

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from users.models import User
+from .models import User
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -8,45 +8,76 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from rest_framework_simplejwt.authentication import JWTAuthentication #jwt 유저 인증 확인
 from rest_framework.permissions import IsAuthenticated #인증 권한 확인
+
+from .serializers import ProfileSerializer
+
+
 # Create your views here.
 
 class ProfileView(APIView):
-    authentication_classes = (JWTAuthentication)
-    permission_classes = (IsAuthenticated)
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    @swagger_auto_schema(
+        tags=["users"],  # Api 이름
+        operation_description="사용자 프로필 api",  # 기능 설명
+        responses={200: "OK", 400: "BAD_REQUEST"},  # 할당된 요청
+    )
     def get(self, request):
-        return Response("user profile")
+        try:
+            user = request.user
+            serializer = ProfileSerializer(user)
+        except Exception as e:
+            return Response({"error" + str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class ProfileEditView(APIView):
-    authentication_classes = (JWTAuthentication)
-    permission_classes = (IsAuthenticated)
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    @swagger_auto_schema(
+        tags=["users"],  # Api 이름
+        operation_description="사용자 프로필 수정",  # 기능 설명
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'username': openapi.Schema(type=openapi.TYPE_STRING),
+                'comment': openapi.Schema(type=openapi.TYPE_STRING),
+            }
+        ),
+        responses={200: "OK", 400: "BAD_REQUEST"},  # 할당된 요청
+    )
     def patch(self, request):
+        user = request.user
+        try:
+            if request.data['username']:
+                user.username = request.data['username']
+            if request.data['comment']:
+                user.comment = request.data['comment']
+        except Exception as e:
+            return Response("error:" + str(e), status=status.HTTP_400_BAD_REQUEST)
         return Response("user profile edit")
 
 class ProfileUploadView(APIView):
-    authentication_classes = (JWTAuthentication)
-    permission_classes = (IsAuthenticated)
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
     @swagger_auto_schema(
         tags=["users"],  # Api 이름
         operation_description="사용자 프로필 업로드",  # 기능 설명
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
-                'username': openapi.Schema(type=openapi.TYPE_STRING),
                 'image': openapi.Schema(type=openapi.TYPE_STRING, format=openapi.FORMAT_BINARY)
             }
         ),
         responses={200: "OK", 400: "BAD_REQUEST"},  # 할당된 요청
     )
     def patch(self, request):
-        username = request.data['username']
+        user = request.user
         image_file = request.data.get('image')
         if not image_file:
             return Response("Image file is required", status=status.HTTP_400_BAD_REQUEST)
         try:
-            user = User.objects.get(username=username)
             user.image = image_file
             user.save()
-            return Response("User profile updated")
-        except User.DoesNotExist:
-            return Response("User does not exist", status=status.HTTP_400_BAD_REQUEST)
-
+        except Exception as e:
+            return Response("error:" + str(e), status=status.HTTP_400_BAD_REQUEST)
+        return Response("User profile updated")
