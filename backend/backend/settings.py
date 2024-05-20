3d1c32ee00adf8d3354137af340d74c1ec222016
이원진 <wonjilee@student.42seoul.kr>
2024-05-20 16:24:45 +0900

    [BE] Chore: user test fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,8 +151,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # JWT
 REST_FRAMEWORK = {
