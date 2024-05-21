5b6cfbf0d33f9da46cac85cd946098badcbcccff
PHJoon <phj9503@gmail.com>
2024-05-21 20:01:55 +0900

    [FE] feat: PlayGame 페이지 추가

---
+++ frontend/src/pages/game/PlayGame.js
@@ -0,0 +1,17 @@
+import PageComponent from '@component/PageComponent';
+import game from '@/utils/game';
+
+class PlayGame extends PageComponent {
+  constructor() {
+    super();
+    this.setTitle('Play Game');
+  }
+
+  async render() {
+  }
+
+  async afterRender() {
+    game();
+  }
+}
+export default PlayGame;
