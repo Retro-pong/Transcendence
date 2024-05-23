71c8c018d14049d371089764718f2cce3b2d4122
PHJoon <phj9503@gmail.com>
2024-05-23 19:57:56 +0900

    [FE] refactor: game내 함수들 분리 및 공 움직임 추가

--- frontend/src/utils/game/game.js
+++ frontend/src/utils/game/game.js
@@ -1,7 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import createMap from './createMap';
-import createGameObject from "@/utils/game/createGameObject";
+import createGameObject from '@/utils/game/createGameObject';
+import eventHandler from '@/utils/game/eventHandler';
 
 function game() {
   const canvas = document.getElementById('gameCanvas');
@@ -12,10 +13,10 @@ function game() {
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(-50, -1, 0);
+  camera.position.set(-40, 0, 0);
 
   const controls = new OrbitControls(camera, canvas);
-  controls.target.set(0, 0, 0);
+  // controls.target.set(0, 0, 0);
   controls.update();
 
   const scene = new THREE.Scene();
@@ -44,10 +45,16 @@ function game() {
     scene.add(light.target);
   }
 
-  const map = createMap(scene);
-  const { ball, redPaddle, bluePaddle } = createGameObject(scene);
+  createMap(scene, controls);
+  createGameObject(scene);
+  eventHandler(canvas, scene, camera, renderer, controls);
 
+  const map = scene.getObjectByName('map');
+  const ball = scene.getObjectByName('ball');
+  const redPaddle = scene.getObjectByName('redPaddle');
+  const bluePaddle = scene.getObjectByName('bluePaddle');
 
+  let ballDirection = 1;
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -69,9 +76,19 @@ function game() {
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
-    if (map) {
-      map.rotation.x += 0.005;
+
+    if (ball) {
+      if (ball.position.x > 23.5 || ball.position.x < -23.5) {
+        ballDirection *= -1;
+      }
+
+      if (ballDirection === 1) {
+        ball.position.x += 0.5;
+      } else {
+        ball.position.x -= 0.5;
+      }
     }
+
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
