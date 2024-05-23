587c8baed85f695e857dd401cb13dd950c1f923d
PHJoon <phj9503@gmail.com>
2024-05-23 17:40:52 +0900

    [FE] fix: map 박스 변경, 시점 설정

--- frontend/src/utils/game/createMap.js
+++ frontend/src/utils/game/createMap.js
@@ -1,32 +1,31 @@
 import * as THREE from 'three';
 
-function createMap(scene) {
+function createMap(scene, controls) {
   const map = new THREE.Object3D();
   scene.add(map);
 
-  const mapBoxGeometry = new THREE.BoxGeometry(50, 10, 10, 4, 0, 4);
+  const mapBoxGeometry = new THREE.BoxGeometry(50, 10, 10, 4, 4, 4);
   const mapBoxMaterial = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
   });
   const mapBox = new THREE.Mesh(mapBoxGeometry, mapBoxMaterial);
-  mapBox.position.set(0, 0, 0);
-  const mapBoxGeometry2 = new THREE.BoxGeometry(50, 10, 10, 4, 4, 0);
-  const mapBoxMaterial2 = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-    reflectivity: 1,
-  });
-  const mapBox2 = new THREE.Mesh(mapBoxGeometry2, mapBoxMaterial2);
+
   mapBox.position.set(0, 0, 0);
 
   map.add(mapBox);
-  map.add(mapBox2);
 
   mapBox.name = 'mapBox';
-  mapBox2.name = 'mapBox2';
 
-  return map;
+  const box3 = new THREE.Box3().setFromObject(mapBox);
+  const boxCenter = new THREE.Vector3();
+  box3.getCenter(boxCenter);
+
+  controls.maxDistance = box3.getSize(new THREE.Vector3()).length();
+  controls.minDistance = 20;
+
+  controls.target.copy(boxCenter);
+  controls.update();
 }
 
 export default createMap;
