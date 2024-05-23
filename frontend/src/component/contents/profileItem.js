f45312ba8196c72ee1355f5cf1dec6fad7b0cdae
PHJoon <phj9503@gmail.com>
2024-05-23 21:08:29 +0900

    [FE] refactor: userProfileInfo 리팩토링

---
+++ frontend/src/component/contents/profileItem.js
@@ -0,0 +1,21 @@
+const profileItem = ({ type, content }) => {
+  if (type === 'comment') {
+    return `
+	  <div class="row">
+	  	<div class="col-4">${type}</div>
+	  	<div class="col-1">:</div>
+	  	<textarea class="col-7 bg-transparent border-0 text-white" style="resize: none" disabled>${content}</textarea>
+	  </div>
+		`;
+  }
+  return `
+	<div class="row">
+		 	<div class="col-4">${type}</div>
+	  	<div class="col-1">:</div>
+	  	<div class="col-7 overflow-scrollbar-x">${content}</div>
+	</div>
+	
+	`;
+};
+
+export default profileItem;
