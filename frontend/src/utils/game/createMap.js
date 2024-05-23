0cfc633a5f2840547f5489c129c6ac582628cfc5
PHJoon <phj9503@gmail.com>
2024-05-23 16:52:35 +0900

    [FE] fix: createMap 수정

--- frontend/src/utils/game/createMap.js
+++ frontend/src/utils/game/createMap.js
@@ -1,28 +1,29 @@
 import * as THREE from 'three';
 
 function createMap(scene) {
-  const mapBoxGeometry = new THREE.BoxGeometry(50, 10, 10, 5, 0, 5);
+  const map = new THREE.Object3D();
+  scene.add(map);
+
+  const mapBoxGeometry = new THREE.BoxGeometry(50, 10, 10, 4, 0, 4);
   const mapBoxMaterial = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
   });
   const mapBox = new THREE.Mesh(mapBoxGeometry, mapBoxMaterial);
   mapBox.position.set(0, 0, 0);
-  const mapBoxGeometry2 = new THREE.BoxGeometry(50, 10, 10, 5, 5, 0);
+  const mapBoxGeometry2 = new THREE.BoxGeometry(50, 10, 10, 4, 4, 0);
   const mapBoxMaterial2 = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
     wireframe: true,
+    reflectivity: 1,
   });
   const mapBox2 = new THREE.Mesh(mapBoxGeometry2, mapBoxMaterial2);
   mapBox.position.set(0, 0, 0);
-  scene.add(mapBox);
-  scene.add(mapBox2);
 
-  const ballGeometry = new THREE.SphereGeometry(1, 32, 16);
-  const ballMaterial = new THREE.MeshPhongMaterial({ color: 0x00ffff });
-  const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-  ball.position.set(0, 0, 0);
-  scene.add(ball);
+  map.add(mapBox);
+  map.add(mapBox2);
+
+  return map;
 }
 
 export default createMap;
