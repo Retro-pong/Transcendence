482bf7e5752e33105e4a43393d79d07213c39aab
PHJoon <phj9503@gmail.com>
2024-05-24 20:30:41 +0900

    [FE] fix: FriendSearchListItem - friend-add-btn -> friend-request-btn 클래스명 변경

--- frontend/src/component/contents/FriendSearchListItem.js
+++ frontend/src/component/contents/FriendSearchListItem.js
@@ -2,7 +2,7 @@ const FriendSearchListItem = ({ nick }) => {
   return `
   <div class="row my-2 px-5 h-25 d-flex justify-content-center align-items-center">
     <div class="col-10 fs-10">${nick}</div>
-    <button class="col-2 friend-add-btn btn btn-no-outline-hover bg-transparent fs-8" data-nick=${nick}>+</button>
+    <button class="col-2 friend-request-btn btn btn-no-outline-hover bg-transparent fs-8" data-nick=${nick}>+</button>
   </div>
   `;
 };
