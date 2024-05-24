9e79c7bb0233953e4ab46a3d1cb206a7d2eba164
PHJoon <phj9503@gmail.com>
2024-05-24 19:46:35 +0900

    [FE] feat: FriendSearchListItem - data-nick 추가

--- frontend/src/component/contents/FriendSearchListItem.js
+++ frontend/src/component/contents/FriendSearchListItem.js
@@ -2,7 +2,7 @@ const FriendSearchListItem = ({ nick }) => {
   return `
   <div class="row my-2 px-5">
     <div class="col-10 fs-10">${nick}</div>
-    <button id="friendAdd" class="btn fs-8 col-2 btn-no-outline-hover bg-transparent">+</button>
+    <button class="btn fs-8 col-2 btn-no-outline-hover bg-transparent" data-nick=${nick}>+</button>
   </div>
   `;
 };
