8c731fe9c00d337f8b5f3579d399e64ed0db9f29
PHJoon <phj9503@gmail.com>
2024-05-24 19:52:26 +0900

    [FE] feat: FriendSearchListItem - 클래스명 추가

--- frontend/src/component/contents/FriendSearchListItem.js
+++ frontend/src/component/contents/FriendSearchListItem.js
@@ -2,7 +2,7 @@ const FriendSearchListItem = ({ nick }) => {
   return `
   <div class="row my-2 px-5">
     <div class="col-10 fs-10">${nick}</div>
-    <button class="btn fs-8 col-2 btn-no-outline-hover bg-transparent" data-nick=${nick}>+</button>
+    <button class="friend-add-btn  btn fs-8 col-2 btn-no-outline-hover bg-transparent" data-nick=${nick}>+</button>
   </div>
   `;
 };
