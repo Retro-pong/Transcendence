9e31665d75368972c5a276b43658e7557411d4e1
PHJoon <phj9503@gmail.com>
2024-05-24 16:30:55 +0900

    [FE] design: EditProfileForm css 수정

--- frontend/src/component/form/EditProfileForm.js
+++ frontend/src/component/form/EditProfileForm.js
@@ -1,23 +1,23 @@
 const EditProfileForm = ({ nick, comment }) => {
   return `
-	<form id="editProfileForm" class="container-fluid" >
-		<div class="row d-flex flex-row p-2 mb-5">
-			<div class="col-5 d-flex align-items-center justify-content-start fs-8">
+	<form id="editProfileForm" class="container-fluid px-5" >
+		<div class="row d-flex flex-row mb-5 px-2 fs-8">
+			<div class="col-5 d-flex align-items-center justify-content-start">
 				<label for="editNickname" class="form-label">NICK</label>
 			</div>
-			<div class="col-1 d-flex align-items-center fs-8">:</div>
-			<div class="col-6 d-flex align-items-center justify-content-center fs-8">
-				<input type="text" id="editNickname" class="form-control h-100 fs-8 bg-transparent" value=${nick} />
+			<div class="col-1 d-flex align-items-center">:</div>
+			<div class="col-6 d-flex align-items-center justify-content-center">
+				<input type="text" id="editNickname" class="form-control h-100 bg-transparent text-white fs-8" value=${nick} />
 			</div>
 		</div>
 		
-		<div class="row d-flex flex-row p-2">
-			<div class="col-5 d-flex align-items-center justify-content-start fs-8">
+		<div class="row d-flex flex-row px-2 fs-8">
+			<div class="col-5 d-flex align-items-center justify-content-start">
 				<label for="editComment" class="form-label">COMMENT</label>
 			</div>
-			<div class="col-1 d-flex align-items-center fs-8">:</div>
-			<div class="col-6 d-flex align-items-center justify-content-center fs-8">
-				<textarea type="text" id="editComment" class="form-control fs-8 bg-transparent">${comment}</textarea>
+			<div class="col-1 d-flex align-items-center">:</div>
+			<div class="col-6 d-flex align-items-center justify-content-center">
+				<textarea type="text" id="editComment" class="form-control bg-transparent fs-8">${comment}</textarea>
 			</div>
 		</div>
 		
