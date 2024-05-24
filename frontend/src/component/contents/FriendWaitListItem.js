c1b721a610971a3f68069136155c02afd239e90a
PHJoon <phj9503@gmail.com>
2024-05-24 20:28:52 +0900

    [FE] feat: FriendWaitListItem - data-nick 추가

--- frontend/src/component/contents/FriendWaitListItem.js
+++ frontend/src/component/contents/FriendWaitListItem.js
@@ -3,10 +3,10 @@ const FriendWaitListItem = ({ nick }) => {
     <div class="row my-2">
       <div class="col-8 d-flex align-items-center justify-content-start fs-8">${nick}</div>
         <div class="col-2 d-flex align-items-center justify-content-center">
-          <button class="friend-accept-btn btn btn-friend-accept rounded-5 fs-8">OK</button>
+          <button class="friend-accept-btn btn btn-friend-accept rounded-5 fs-8" data-nick=${nick}>OK</button>
         </div>
       <div class="col-2 d-flex align-items-center justify-content-center">
-        <button class="friend-reject-btn btn btn-friend-reject rounded-5 fs-8">NO</button>
+        <button class="friend-reject-btn btn btn-friend-reject rounded-5 fs-8" data-nick=${nick}>NO</button>
       </div>
     </div>`;
 };
