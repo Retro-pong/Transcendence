5215a6ec6981c08cd382472da56ce8ec8c1ec78b
PHJoon <phj9503@gmail.com>
2024-05-28 20:43:01 +0900

    [FE] design: game - ballPlane 디자인 수정

--- frontend/src/utils/game/createGameObject.js
+++ frontend/src/utils/game/createGameObject.js
@@ -9,15 +9,48 @@ function createGameObject(scene) {
   const ball = new THREE.Mesh(ballGeometry, ballMaterial);
   ball.position.set(0, 0, 0);
 
-  const ballPlaneGeometry = new THREE.PlaneGeometry(15, 10);
-  const ballPlaneMaterial = new THREE.MeshBasicMaterial({
-    color: 0x00ffff,
-    transparent: true,
-    opacity: 0.3,
-  });
-  ballPlaneMaterial.side = THREE.DoubleSide;
-  const ballPlane = new THREE.Mesh(ballPlaneGeometry, ballPlaneMaterial);
-  ballPlane.rotation.y = Math.PI / 2;
+  const planeY = 10;
+  const planeZ = 15;
+
+  const lines = [
+    [new THREE.Vector3(0, planeY / 2, -planeZ / 2),
+    new THREE.Vector3(0, planeY / 2, planeZ / 2)],
+    [new THREE.Vector3(0, planeY / 2, planeZ / 2),
+    new THREE.Vector3(0, -planeY / 2, planeZ / 2)],
+    [new THREE.Vector3(0, -planeY / 2, planeZ / 2),
+    new THREE.Vector3(0, -planeY / 2, -planeZ / 2)],
+    [new THREE.Vector3(0, -planeY / 2, -planeZ / 2),
+    new THREE.Vector3(0, planeY / 2, -planeZ / 2)],
+  ];
+
+  const lineWidth = 0.5;
+  const lineGeometry1 = new THREE.BoxGeometry(lineWidth, planeY + lineWidth, lineWidth);
+  const lineGeometry2 = new THREE.BoxGeometry(lineWidth, planeY + lineWidth, lineWidth);
+  const lineGeometry3 = new THREE.BoxGeometry(lineWidth, lineWidth, planeZ + lineWidth);
+  const lineGeometry4 = new THREE.BoxGeometry(lineWidth, lineWidth, planeZ + lineWidth);
+  const ballPlaneMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, opacity: 0.5});
+
+  const ballPlaneLine1 = new THREE.Mesh(lineGeometry1, ballPlaneMaterial);
+  const ballPlaneLine2 = new THREE.Mesh(lineGeometry2, ballPlaneMaterial);
+  const ballPlaneLine3 = new THREE.Mesh(lineGeometry3, ballPlaneMaterial);
+  const ballPlaneLine4 = new THREE.Mesh(lineGeometry4, ballPlaneMaterial);
+
+  ballPlaneLine1.position.z = planeZ / 2;
+  ballPlaneLine2.position.z = -planeZ / 2;
+  ballPlaneLine3.position.y = planeY / 2;
+  ballPlaneLine4.position.y = -planeY / 2;
+
+  ballPlaneLine1.rotation.y = Math.PI / 2;
+  ballPlaneLine2.rotation.y = Math.PI / 2;
+  ballPlaneLine3.rotation.z = Math.PI / 2;
+  ballPlaneLine4.rotation.z = Math.PI / 2;
+
+  const ballPlane = new THREE.Group();
+  ballPlane.add(ballPlaneLine1);
+  ballPlane.add(ballPlaneLine2);
+  ballPlane.add(ballPlaneLine3);
+  ballPlane.add(ballPlaneLine4);
+
   ballPlane.position.set(0, 0, 0);
   ballPlane.name = 'ballPlane';
   scene.getObjectByName('map').add(ballPlane);
