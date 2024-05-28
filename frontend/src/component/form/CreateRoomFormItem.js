98eabdd50fd80bc27ee33a744b21c5963c24b586
PHJoon <phj9503@gmail.com>
2024-05-29 04:54:15 +0900

    [FE] fix: CreateRoomFormItem - 타이틀 label 태그 조건부 생성으로 수정

--- frontend/src/component/form/CreateRoomFormItem.js
+++ frontend/src/component/form/CreateRoomFormItem.js
@@ -1,8 +1,12 @@
-const CreateRoomFormItem = ({ labelFor, labelContent, content }) => {
+const CreateRoomFormItem = ({ titleId, title, content, label = false }) => {
+  const titleItem = label
+    ? `<label for=${titleId} class="form-label">${title}:</label>`
+    : `<div id=${titleId}>${title}:</div>`;
+
   return `
 		<div class="row h-25 w-100 fs-13 mt-4 d-flex justify-content-center align-items-center">
 			<div class="col-4 h-100 fs-13 d-flex justify-content-center align-items-center">
-				<label for=${labelFor} class="form-label">${labelContent}:</label>
+				${titleItem}
 			</div>
 			<div class="col-8 h-100 p-0 d-flex flex-row justify-content-center align-items-center">
 				${content}
