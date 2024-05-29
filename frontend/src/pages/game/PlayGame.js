b28e463eb35686b125e7b16cffe3559e85d0f671
PHJoon <phj9503@gmail.com>
2024-05-29 21:42:49 +0900

    [FE] feat: playGame 페이지 type 세팅값 추가

--- frontend/src/pages/game/PlayGame.js
+++ frontend/src/pages/game/PlayGame.js
@@ -6,6 +6,7 @@ class PlayGame extends PageComponent {
     super();
     this.setTitle('Play Game');
     this.settings = {
+      type: 'multi',
       ball: 0x0000ff,
       speed: 1,
       map: 'mountain',
@@ -15,6 +16,14 @@ class PlayGame extends PageComponent {
   async render() {}
 
   async afterRender() {
+    if (this.settings.type === 'local') {
+      document.getElementById('gameCanvasLocalOne').classList.remove('d-none');
+      document.getElementById('gameCanvasLocalTwo').classList.remove('d-none');
+    }
+    if (this.settings.type === 'multi') {
+      document.getElementById('gameCanvasMulti').classList.remove('d-none');
+    }
+
     game(this.settings);
 
     document.getElementById('mapTest').addEventListener('click', (e) => {
