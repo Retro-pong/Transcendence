aaf7639001a315aa8c7492dc3b85b2c5aa975eb7
PHJoon <phj9503@gmail.com>
2024-05-29 22:53:05 +0900

    [FE] chore: lint 적용

--- frontend/src/utils/game/rendering.js
+++ frontend/src/utils/game/rendering.js
@@ -1,30 +1,30 @@
 function rendering(renderer, scene, camera, type) {
-	if (type === 'multi') {
-		renderer.render(scene, camera.multi);
-	}
-	if (type === 'local') {
-		// 첫번째 뷰 렌더링
-		renderer.setViewport(0, 0, window.innerWidth / 2, window.innerHeight);
-		renderer.setScissor(0, 0, window.innerWidth / 2, window.innerHeight);
-		renderer.setScissorTest(true);
-		renderer.render(scene, camera.red);
+  if (type === 'multi') {
+    renderer.render(scene, camera.multi);
+  }
+  if (type === 'local') {
+    // 첫번째 뷰 렌더링
+    renderer.setViewport(0, 0, window.innerWidth / 2, window.innerHeight);
+    renderer.setScissor(0, 0, window.innerWidth / 2, window.innerHeight);
+    renderer.setScissorTest(true);
+    renderer.render(scene, camera.red);
 
-		// 두 번째 뷰 렌더링
-		renderer.setViewport(
-			window.innerWidth / 2,
-			0,
-			window.innerWidth / 2,
-			window.innerHeight
-		);
-		renderer.setScissor(
-			window.innerWidth / 2,
-			0,
-			window.innerWidth / 2,
-			window.innerHeight
-		);
-		renderer.setScissorTest(true);
-		renderer.render(scene, camera.blue);
-	}
+    // 두 번째 뷰 렌더링
+    renderer.setViewport(
+      window.innerWidth / 2,
+      0,
+      window.innerWidth / 2,
+      window.innerHeight
+    );
+    renderer.setScissor(
+      window.innerWidth / 2,
+      0,
+      window.innerWidth / 2,
+      window.innerHeight
+    );
+    renderer.setScissorTest(true);
+    renderer.render(scene, camera.blue);
+  }
 }
 
 export default rendering;
