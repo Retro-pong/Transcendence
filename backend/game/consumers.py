7bd1537319e9397307cd5448acfafe732bb814de
이원진 <wonjilee@student.42seoul.kr>
2024-05-29 16:00:11 +0900

    [BE]feat: game api add

---
+++ backend/game/consumers.py
@@ -0,0 +1,8 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+
+    async def receive(self, text_data):
+
+    async def disconnect(self):
