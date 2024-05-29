f2973ee5a9a29eedf1f59b6dee582f72a973684e
PHJoon <phj9503@gmail.com>
2024-05-30 00:30:02 +0900

    [FE] feat: eventHandler 로컬, 멀티인 경우 분리 추가

--- frontend/src/utils/game/eventHandler.js
+++ frontend/src/utils/game/eventHandler.js
@@ -1,4 +1,4 @@
-function eventHandler(canvas, scene) {
+function eventHandler(canvas, scene, type, side) {
   const redPaddle = scene.getObjectByName('redPaddle');
   const bluePaddle = scene.getObjectByName('bluePaddle');
 
@@ -16,20 +16,22 @@ function eventHandler(canvas, scene) {
 
   // TODO: 멀티 플레이 시 키보드 컨트롤 방향키만 사용하도록 수정, 패들도 하나 받기
   // 키보드 컨트롤
+  // wasd -> 로컬에서만 사용
   canvas.addEventListener('keydown', (e) => {
-    console.log(e.code);
     // 방향키
-    if (e.code === 'KeyW') {
-      keyPressed.w = true;
-    }
-    if (e.code === 'KeyS') {
-      keyPressed.s = true;
-    }
-    if (e.code === 'KeyA') {
-      keyPressed.a = true;
-    }
-    if (e.code === 'KeyD') {
-      keyPressed.d = true;
+    if (type === 'local') {
+      if (e.code === 'KeyW') {
+        keyPressed.w = true;
+      }
+      if (e.code === 'KeyS') {
+        keyPressed.s = true;
+      }
+      if (e.code === 'KeyA') {
+        keyPressed.a = true;
+      }
+      if (e.code === 'KeyD') {
+        keyPressed.d = true;
+      }
     }
     if (e.code === 'ArrowUp') {
       keyPressed.up = true;
@@ -56,6 +58,12 @@ function eventHandler(canvas, scene) {
     if (e.code === 'ArrowRight') keyPressed.right = false;
   });
 
+  let paddle;
+  if (type === 'local') {
+    paddle = bluePaddle;
+  } else {
+    paddle = side === 'red' ? redPaddle : bluePaddle;
+  }
   const movePaddle = () => {
     const move = 0.2;
     if (keyPressed.w) {
@@ -70,17 +78,30 @@ function eventHandler(canvas, scene) {
     if (keyPressed.d) {
       if (redPaddle.position.z > -7.5) redPaddle.position.z -= move;
     }
+    // multi, local 구분
     if (keyPressed.up) {
-      if (bluePaddle.position.y < 5) bluePaddle.position.y += move;
+      if (paddle === bluePaddle && bluePaddle.position.y < 5)
+        bluePaddle.position.y += move;
+      if (paddle === redPaddle && redPaddle.position.y < 5)
+        redPaddle.position.y += move;
     }
     if (keyPressed.down) {
-      if (bluePaddle.position.y > -5) bluePaddle.position.y -= move;
+      if (paddle === bluePaddle && bluePaddle.position.y > -5)
+        bluePaddle.position.y -= move;
+      if (paddle === redPaddle && redPaddle.position.y > -5)
+        redPaddle.position.y -= move;
     }
     if (keyPressed.left) {
-      if (bluePaddle.position.z > -7.5) bluePaddle.position.z -= move;
+      if (paddle === bluePaddle && bluePaddle.position.z > -7.5)
+        bluePaddle.position.z -= move;
+      if (paddle === redPaddle && redPaddle.position.z < 7.5)
+        redPaddle.position.z += move;
     }
     if (keyPressed.right) {
-      if (bluePaddle.position.z < 7.5) bluePaddle.position.z += move;
+      if (paddle === bluePaddle && bluePaddle.position.z < 7.5)
+        bluePaddle.position.z += move;
+      if (paddle === redPaddle && redPaddle.position.z > -7.5)
+        redPaddle.position.z -= move;
     }
   };
 
