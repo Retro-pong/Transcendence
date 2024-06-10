707dc1cee2ea06edcb8cc06c2439848e8c0b2082
이원진 <wonjilee@student.42seoul.kr>
2024-06-10 16:05:35 +0900

    [BE]chore: redis ping delete

--- backend/cmd.sh
+++ backend/cmd.sh
@@ -5,7 +5,6 @@ python manage.py migrate --noinput
 echo "from users.models import User; User.objects.create_superuser('test', 'tttest@example.com', 'test')" | python manage.py shell
 #python manage.py runserver 0.0.0.0:8000
 redis-server --daemonize yes
-redis-cli ping
 daphne -b 0.0.0.0 -p 8000 backend.asgi:application
 
 
