0434dd667a87ae8303580f1a020456433f35aa1c
hyobb109 <hyobb109@gmail.com>
2024-06-13 17:39:13 +0900

    [FE] fix: join room modal hide 수정

--- frontend/src/pages/game/JoinRoom.js
+++ frontend/src/pages/game/JoinRoom.js
@@ -3,6 +3,7 @@ import NavLink from '@component/navigation/NavLink';
 import BasicButton from '@component/button/BasicButton';
 import ModalComponent from '@component/modal/ModalComponent';
 import GameRoomInfo from '@component/contents/GameRoomInfo';
+import { Modal } from 'bootstrap';
 import Fetch from '@/utils/Fetch';
 import ToastHandler from '@/utils/ToastHandler';
 import Router from '@/utils/Router';
@@ -70,7 +71,7 @@ class JoinRoom extends PageComponent {
       const navBtn = document.querySelector('#roomInfoModal .modal-footer a');
       navBtn.addEventListener('click', async (e) => {
         e.preventDefault();
-        roomInfoModal.hide();
+        Modal.getInstance(roomInfoModal).hide();
         await Router.navigateTo(navBtn.href);
       });
       modalBody.innerHTML = GameRoomInfo(roomInfo);
