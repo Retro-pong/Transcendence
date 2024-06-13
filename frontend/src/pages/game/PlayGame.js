424767e36069edd6d35fef44a683ef3f84f42ff8
PHJoon <phj9503@gmail.com>
2024-06-13 17:40:12 +0900

    [FE] feat: 테스트 코드 삭제, 페이지 속성 추가

--- frontend/src/pages/game/PlayGame.js
+++ frontend/src/pages/game/PlayGame.js
@@ -1,10 +1,13 @@
 import PageComponent from '@component/PageComponent';
 import game from '@/utils/game/game';
+import socketManager from '@/utils/SocketManager';
 
 class PlayGame extends PageComponent {
   constructor() {
     super();
     this.setTitle('Play Game');
+    const params = new URLSearchParams(document.location.search);
+    this.roomId = params.get('id');
     this.settings = {
       mode: 'local',
       side: 'blue',
@@ -12,6 +15,7 @@ class PlayGame extends PageComponent {
       speed: 1,
       map: 'mountain',
     };
+    this.gameSocket = socketManager.createSocket(`/norma_game/${this.roomId}/`);
   }
 
   async render() {}
@@ -32,67 +36,6 @@ class PlayGame extends PageComponent {
   async afterRender() {
     this.setScoreBox();
     game(this.settings);
-
-    // test 버튼들
-    document.getElementById('openTest').addEventListener('click', (e) => {
-      const testButtons = document.getElementById('testButtons');
-      if (testButtons.classList.contains('d-none')) {
-        testButtons.classList.remove('d-none');
-      } else {
-        testButtons.classList.add('d-none');
-      }
-    });
-
-    document.getElementById('mapTest').addEventListener('click', (e) => {
-      if (this.settings.map === 'mountain') {
-        this.settings.map = 'pixel';
-      } else if (this.settings.map === 'pixel') {
-        this.settings.map = 'horizon';
-      } else {
-        this.settings.map = 'mountain';
-      }
-      document.getElementById('mapTestVal').innerText = this.settings.map;
-      game(this.settings);
-    });
-
-    document.getElementById('colorTest').addEventListener('click', (e) => {
-      if (this.settings.ball === 0x0000ff) {
-        this.settings.ball = 0xff0000;
-      } else if (this.settings.ball === 0xff0000) {
-        this.settings.ball = 0x00ff00;
-      } else {
-        this.settings.ball = 0x0000ff;
-      }
-      document.getElementById('colorTestVal').innerText = this.settings.ball;
-      game(this.settings);
-    });
-    document.getElementById('speedTest').addEventListener('click', (e) => {
-      if (this.settings.speed < 5) {
-        this.settings.speed += 1;
-      } else {
-        this.settings.speed = 1;
-      }
-      document.getElementById('speedTestVal').innerText = this.settings.speed;
-      game(this.settings);
-    });
-    document.getElementById('gameTypeTest').addEventListener('click', (e) => {
-      if (this.settings.mode === 'local') {
-        this.settings.mode = 'multi';
-        this.settings.side = 'blue';
-      } else if (
-        this.settings.mode === 'multi' &&
-        this.settings.side === 'blue'
-      ) {
-        this.settings.side = 'red';
-      } else if (
-        this.settings.mode === 'multi' &&
-        this.settings.side === 'red'
-      ) {
-        this.settings.mode = 'local';
-      }
-      this.setScoreBox();
-      game(this.settings);
-    });
   }
 }
 export default PlayGame;
