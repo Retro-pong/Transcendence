1e8ababa6064f6c2dcf609f2a058151908b576a5
hyobb109 <hyobb109@gmail.com>
2024-06-14 01:11:07 +0900

    [FE] refactor: PlayerInfo 삭제

--- frontend/src/component/card/PlayerCard.js
+++ frontend/src/component/card/PlayerCard.js
@@ -1,9 +1,26 @@
-import PlayerInfo from '@component/card/PlayerInfo';
-
 const PlayerCard = ({ id, name, profileImg, win, lose }) => {
+  const score = name ? `${win}W/${lose}L` : '...';
+
   return `
       <div class="row game-player-card-border g-3">
-        ${PlayerInfo({ id, name, profileImg, win, lose })}
+        <div class="col-md-8 align-self-center text-start fs-7">
+          <div class="text-break">
+            <div class="row">
+              <span id='player${id}-id' class="text-info text-opacity-75">Player ${id}</span>
+            </div>
+            <div class="row">
+              <span id='player${id}-name' >${name || 'waiting...'}</span>
+            </div>
+            <div class="row">
+              <span id='player${id}-score' >${score}</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 align-content-center">
+          <div id='player${id}-img' class="ratio ratio-1x1">
+            ${profileImg ? `<img src="${profileImg}" onerror="this.src='/img/profile_fallback.jpg';" class="img-fluid" alt="profile" style="object-fit: cover;" />` : ''}
+          </div>
+        </div>
       </div>
   `;
 };
