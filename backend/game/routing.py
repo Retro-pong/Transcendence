c6a41db6c2e5aaa61f55f1f0329b3a97a8b313b5
Subin Lee <ddubi701@gmail.com>
2024-06-15 19:49:37 +0900

    [BE] Chore: 주석 처리

--- backend/game/routing.py
+++ backend/game/routing.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
 
-from .consumers import NormalGameConsumer, TournamentGameConsumer
+from .consumers import NormalGameConsumer  # , TournamentGameConsumer
 
 
 websocket_urlpatterns = [
     re_path(r"ws/normal_game/(?P<game_id>\w+)/$", NormalGameConsumer.as_asgi()),
-    re_path(r"ws/tournament_game/(?P<game_id>\w+)/$", TournamentGameConsumer.as_asgi()),
+    # re_path(r"ws/tournament_game/(?P<game_id>\w+)/$", TournamentGameConsumer.as_asgi()),
 ]
