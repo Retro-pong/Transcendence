fefc81b1eeff89a965110a79903f63efac2a7af2
Subin Lee <ddubi701@gmail.com>
2024-06-15 19:46:36 +0900

    [BE] Chore: fixed format

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -6,7 +6,7 @@ import asyncio
 from backend.middleware import JWTAuthMiddleware
 
 
-class GameConsumer(AsyncJsonWebsocketConsumer):
+class NormalGameConsumer(AsyncJsonWebsocketConsumer):
     games: dict[str, Game] = {}
     games_lock = asyncio.Lock()
 
@@ -21,7 +21,7 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
             self.channel_name,  # Consumer 채널 이름
         )
 
-    async def receive_json(self, content) -> None:
+    async def receive_json(self, content: dict) -> None:
         if content["type"] == "access":
             try:
                 self.result = await self.get_game_result()
@@ -30,18 +30,18 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
             await self.user_access(content)
         # Game이 존재할 때만 명령처리
         else:
-            match = GameConsumer.games[self.game_id]
+            match = NormalGameConsumer.games[self.game_id]
             player = match.get_players()[self.color]
-            async with GameConsumer.games_lock:
+            async with NormalGameConsumer.games_lock:
                 if content["type"] == "ready":
                     if match.set_ready(player):
                         self.loop = asyncio.create_task(self.game_loop(player))
             if content["type"] == "move":
                 player.set_pos(content["y"], content["z"])
 
-    async def game_loop(self, player) -> None:
+    async def game_loop(self, player: "Player") -> None:
         while True:
-            match = GameConsumer.games[self.game_id]
+            match = NormalGameConsumer.games[self.game_id]
             if not match:
                 await self.send_json({"error": "Game not found"})
                 return
@@ -65,15 +65,15 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
                         "data_type": "result",
                     },
                 )
-                del GameConsumer.games[self.game_id]
+                del NormalGameConsumer.games[self.game_id]
                 break
 
-    async def broadcast_users(self, event):
+    async def broadcast_users(self, event: dict) -> None:
         data = event["data"]
         data["type"] = event["data_type"]
         await self.send_json(data)
 
-    async def disconnect(self, close_code) -> None:
+    async def disconnect(self, close_code: int) -> None:
         await self.channel_layer.group_send(
             self.game_id,
             {
@@ -81,15 +81,15 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
             },
         )
 
-    async def close_group(self, event) -> None:
+    async def close_group(self, event: dict) -> None:
         await self.send_json({"type": "exit"})
         await self.channel_layer.group_discard(self.game_id, self.channel_name)
 
-    async def user_access(self, content) -> None:
+    async def user_access(self, content: dict) -> None:
         token = content["token"]
         try:
             self.user = await JWTAuthMiddleware.get_user(token)
-        except Exception as e:
+        except:
             await self.send_json({"access": "User invalid or expired."})
             return
         if not self.user.is_authenticated:
@@ -97,10 +97,12 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
             return
         else:
             await self.send_json({"access": "Access successful."})
+
         # Game 객체 생성
-        if self.game_id not in GameConsumer.games:
-            GameConsumer.games[self.game_id] = Game(self.result.game_speed)
-        match = GameConsumer.games[self.game_id]
+        if self.game_id not in NormalGameConsumer.games:
+            NormalGameConsumer.games[self.game_id] = Game(self.result.game_speed)
+        match = NormalGameConsumer.games[self.game_id]
+
         # 들어온 순서대로 red, blue 배정
         if match.p1 is None:
             match.p1 = Player(type="red", nick=self.user.username)
@@ -120,7 +122,7 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         return GameResult.objects.get(id=self.game_id)
 
     @database_sync_to_async
-    def save_game_result(self, match):
+    def save_game_result(self, match: Game) -> None:
         GameResult = apps.get_model("game", "GameResult")
         result = GameResult.objects.get(id=self.game_id)
         result.winner = match.winner
@@ -131,4 +133,53 @@ class GameConsumer(AsyncJsonWebsocketConsumer):
         result.save()
 
 
-# class GameTournamentConsumer(AsyncJsonWebsocketConsumer):
+# class TournamentGameConsumer(NormalGameConsumer):
+#     games: dict[str, Game] = {}
+#     games_lock = asyncio.Lock()
+#
+#     async def receive_json(self, content) -> None:
+#         if content["type"] == "access":
+#             try:
+#                 self.result = await self.get_game_result()
+#             except Exception as e:
+#                 await self.send_json({"error": str(e)})
+#             await self.user_access(content)
+#         # Game이 존재할 때만 명령처리
+#         else:
+#             match = NormalGameConsumer.games[self.game_id]
+#             player = match.get_players()[self.color]
+#             async with NormalGameConsumer.games_lock:
+#                 if content["type"] == "ready":
+#                     if match.set_ready(player):
+#                         self.loop = asyncio.create_task(self.game_loop(player))
+#             if content["type"] == "move":
+#                 player.set_pos(content["y"], content["z"])
+#
+#     async def game_loop(self, player) -> None:
+#         while True:
+#             match = NormalGameConsumer.games[self.game_id]
+#             if not match:
+#                 await self.send_json({"error": "Game not found"})
+#                 return
+#             await asyncio.sleep(0.03)
+#             match.game_render(player)
+#             await self.channel_layer.group_send(
+#                 self.game_id,
+#                 {
+#                     "type": "broadcast_users",
+#                     "data": match.game_data(),
+#                     "data_type": "render",
+#                 },
+#             )
+#             if match.winner:
+#                 await self.save_game_result(match)
+#                 await self.channel_layer.group_send(
+#                     self.game_id,
+#                     {
+#                         "type": "broadcast_users",
+#                         "data": match.result_data(),
+#                         "data_type": "result",
+#                     },
+#                 )
+#                 del NormalGameConsumer.games[self.game_id]
+#                 break
--- backend/game/routing.py
+++ backend/game/routing.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from .consumers import GameConsumer  # GameTournamentConsumer
+from .consumers import NormalGameConsumer, TournamentGameConsumer
+
 
 websocket_urlpatterns = [
-    re_path(r"ws/normal_game/(?P<game_id>\w+)/$", GameConsumer.as_asgi()),
-    # re_path(r'ws/tournament_game/(?P<game_id>\w+)/$', GameTournamentConsumer.as_asgi()),
+    re_path(r"ws/normal_game/(?P<game_id>\w+)/$", NormalGameConsumer.as_asgi()),
+    re_path(r"ws/tournament_game/(?P<game_id>\w+)/$", TournamentGameConsumer.as_asgi()),
 ]
