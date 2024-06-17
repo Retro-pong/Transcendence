3fed5f7732f293a3ca8228387dc8591a93d17596
Subin Lee <ddubi701@gmail.com>
2024-06-17 20:30:38 +0900

    [BE] Chore: 오타 수정

--- backend/game/routing.py
+++ backend/game/routing.py
@@ -1,10 +1,10 @@
 from django.urls import re_path
 
-from .consumers import NormalGameConsumer, SemiFinalGameConsumer
+from .consumers import NormalGameConsumer, SemiFinalGameConsumer, FinalGameConsumer
 
 
 websocket_urlpatterns = [
     re_path(r"ws/normal_game/(?P<game_id>\w+)/$", NormalGameConsumer.as_asgi()),
     re_path(r"ws/semi_final_game/(?P<game_id>\w+)/$", SemiFinalGameConsumer.as_asgi()),
-    re_path(r"ws/final_game/(?P<game_id>\w+)/$", SemiFinalGameConsumer.as_asgi()),
+    re_path(r"ws/final_game/(?P<game_id>\w+)/$", FinalGameConsumer.as_asgi()),
 ]
