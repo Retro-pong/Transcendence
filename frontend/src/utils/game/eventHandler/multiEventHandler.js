32d99bf7e53685103ff3ae51eae342fcfc4d27c3
PHJoon <phj9503@gmail.com>
2024-06-17 14:02:47 +0900

    [FE] refactor: 이벤트 핸들러 리팩토링, 이벤트 폐기하는 반환값 추가

--- frontend/src/utils/game/eventHandler/multiEventHandler.js
+++ frontend/src/utils/game/eventHandler/multiEventHandler.js
@@ -6,8 +6,8 @@ function multiEventHandler(canvas, scene, camera) {
 
   canvas.focus();
 
-  // 마우스 컨트롤
-  canvas.addEventListener('mousemove', (e) => {
+  // 마우스 이벤트 핸들러 함수
+  function handleMouseMove(e) {
     const x = e.clientX;
     const y = e.clientY;
 
@@ -27,7 +27,18 @@ function multiEventHandler(canvas, scene, camera) {
     if (socketManager.gameSocket.readyState === WebSocket.OPEN) {
       socketManager.gameSocket.send(JSON.stringify(message));
     }
-  });
+  }
+
+  // 마우스 이벤트 리스너 등록
+  canvas.focus();
+  canvas.addEventListener('mousemove', handleMouseMove);
+
+  // 애플리케이션 종료 시 이벤트 리스너 제거
+  function removeEventListener() {
+    canvas.removeEventListener('mousemove', handleMouseMove);
+    // 추가 자원 해제 코드
+  }
+  return removeEventListener;
 }
 
 export default multiEventHandler;
