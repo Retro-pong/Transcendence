f926ce81c97d55cf1c40bb773262b72822ad02f3
Subin Lee <ddubi701@gmail.com>
2024-06-24 13:43:53 +0900

    [BE] Chore: 코드 정리

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,7 +1,7 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.apps import apps
-from .modules import Player, Game, WAIT, READY, END, DISCONNECTED
+from .modules import Player, Game, READY, END, DISCONNECTED
 import asyncio
 from backend.middleware import JWTAuthMiddleware
 
