ee38d932b86a30e013e06151188aa2c665906329
Wonjin Lee(이원진) <88661835+inwoo0115@users.noreply.github.com>
2024-06-24 15:03:35 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,7 @@ from datetime import timedelta
 from pathlib import Path
 import environ
 import os
+# import dj_database_url # postgre db url
 
 BASE_DIR_PATH = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
