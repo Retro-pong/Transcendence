5479945701560fa1cb2854d842e5c852f4b80f74
PHJoon <phj9503@gmail.com>
2024-06-30 12:11:50 +0900

    [FE] feat: data-add-btn-target 데이터 추가

--- frontend/src/component/contents/FriendSearchListItem.js
+++ frontend/src/component/contents/FriendSearchListItem.js
@@ -1,8 +1,8 @@
 const FriendSearchListItem = ({ nick }) => {
   return `
-  <div class="row my-2 px-5 h-25 d-flex justify-content-center align-items-center">
-    <div class="col-10 fs-10">${nick}</div>
-    <button class="col-2 friend-request-btn btn btn-no-outline-hover bg-transparent fs-8" data-nick=${nick}>+</button>
+  <div class="row my-2 px-4 d-flex justify-content-center align-items-center">
+    <div class="col-10 fs-10 text-wrap text-break" data-add-btn-target=${nick}>${nick}</div>
+    <button class="col-2 friend-request-btn btn btn-no-outline-hover fs-8" data-nick=${nick}>+</button>
   </div>
   `;
 };
