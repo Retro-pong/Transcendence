d62acfd97f623792b28340de35ff51df7e7a361f
Subin Lee <ddubi701@gmail.com>
2024-07-01 21:35:41 +0900

    [BE] Feat: 메일 제목 추가, 내용 수정

--- backend/login/utils.py
+++ backend/login/utils.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from .models import TFA
 from users.models import User
 from django.conf import settings
@@ -15,13 +17,37 @@ def send_verification_code(email) -> bool:
     TFA.objects.create(email=email, code=code)  # DB에 저장
 
     try:
+        # Set up the SMTP server
         smtp = smtplib.SMTP(settings.EMAIL_HOST, int(settings.EMAIL_PORT))
         smtp.starttls()
         smtp.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+
+        # Create the email
+        msg = MIMEMultipart("alternative")
+        msg["Subject"] = "Verification Code"
+        msg["From"] = settings.EMAIL_HOST_USER
+        msg["To"] = email
+
+        # Create the body of the email with HTML
+        html_content = f"""
+        <html>
+        <body>
+            <p style="font-size: 16px; color: #333;">
+                Your verification code is <strong style="color: #ea04ec;">{code}</strong>
+                </br></br>Enjoy your <strong style="color: #02ffff;">PONG</strong><b>!</b>
+            </p>
+        </body>
+        </html>
+        """
+
+        # Attach the HTML content to the email
+        msg.attach(MIMEText(html_content, "html"))
+
+        # Send the email
         smtp.sendmail(
             settings.EMAIL_HOST_USER,
             email,
-            f"Your verification code is <{code}>",
+            msg.as_string(),
         )
         smtp.quit()
         return True
