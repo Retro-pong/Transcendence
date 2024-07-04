9c34ba13e4ca77483b2515dda5a02b5a3504015a
이원진 <wonjilee@student.42seoul.kr>
2024-07-04 18:40:19 +0900

    [BE]fix: 오타수정

--- backend/cmd.sh
+++ backend/cmd.sh
@@ -2,6 +2,7 @@
 
 python manage.py makemigrations
 python manage.py migrate --noinput
+#super user 생성
 #echo "from users.models import User; User.objects.create_superuser('test123', 'test123@example123.com', 'test123')" | python manage.py shell
 redis-server --daemonize yes
 daphne -b 0.0.0.0 -p 8000 backend.asgi:application
